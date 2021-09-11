deb7b1960ffbfb688fd7ef62fd9196ac1a28be3d
John <ujin_slav@mail.ru>
2021-09-11 15:51:53 +0300

    searchform

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -14,26 +14,51 @@ import {
 const SearchForm = () => {
     return (
         <div>
-            <Container>
-                <Card>
+            <Container className="mb-3 mt-3">
                 <Form>
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Номер закупки</Form.Label>
-                            <Form.Control type="email" placeholder="Номер закупки" />
+                            <Form.Control type="numberOrder" placeholder="Номер закупки" />
                         </Form.Group>
                         <Form.Group as={Col} controlId="formGridPassword">
                             <Form.Label>Регион заказчика</Form.Label>
                             <Form.Control type="regionClient" placeholder="Регион заказчика" />
                         </Form.Group>
-                        <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label >Максимальная цена</Form.Label>
-                                <Form.Control type="maxPrice" className="side-by-side" placeholder="Максимальная цена" />
-                                <Form.Control type="maxPrice" className="side-by-side" placeholder="Максимальная цена" />
+                        <Form.Group  as={Col} controlId="formGridPassword">
+                            <Form.Label>Максимальная цена</Form.Label>
+                            <div class="input-group">
+                                <input type="text" class="form-control input-sm" value="От" />
+                                <span class="input-group-btn" style={{width:"0px"}}></span>
+                                <input type="text" class="form-control input-sm" value="До" />
+                            </div>
                         </Form.Group>
-                    </Row>               
+                    </Row> 
+                    <Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Наименование закупки</Form.Label>
+                            <Form.Control type="nameOrder" placeholder="Наименование" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Наименование или ИНН заказчика</Form.Label>
+                            <Form.Control type="nameClient" placeholder="Наименование или ИНН заказчика" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Макисмальный срок поставки</Form.Label>
+                            <div class="input-group">
+                                <input type="text" class="form-control input-sm" value="От" />
+                                <span class="input-group-btn" style={{width:"0px"}}></span>
+                                <input type="text" class="form-control input-sm" value="До" />
+                            </div>
+                    </Form.Group>
+                    </Row>   
+                    <Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Классификатор</Form.Label>
+                            <Form.Control type="class" placeholder="Классификатор" />
+                    </Form.Group>
+                    </Row>                   
                 </Form>  
-                </Card> 
             </Container>
         </div>
     );
