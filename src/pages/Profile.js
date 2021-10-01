a922e676448e58f14d144bfed489e6d0ecfb95ba
John <ujin_slav@mail.ru>
2021-10-01 15:53:02 +0300

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,51 +1,119 @@
-import {React, useEffect,useContext} from 'react';
-import {Card, Table, Col, Container, Row, Lable} from "react-bootstrap";
+import {React, useEffect,useContext,useState} from 'react';
+import {Card, Table, Col, Container, Row, Lable,Form,Button} from "react-bootstrap";
 import {Context} from "../index";
 import {observer} from "mobx-react-lite";
 
 const Profile =  observer(() => {
 
     const {user} = useContext(Context);  
-    
+    const[profile,setProfile] = useState( {
+        data: {
+            name: user.user.name,
+            nameOrg:  user.user.nameOrg,
+            adressOrg: user.user.adressOrg,
+            telefon: user.user.telefon,
+            inn: user.user.inn,
+            fiz: user.user.fiz
+          },
+          formErrors: {
+            name: "",
+            nameOrg: "",
+            adressOrg: "",
+            telefon: "",
+            Inn: "",
+            fiz: ""
+          }
+      }
+    );
+
+    const handleChange = (e) =>{
+        e.preventDefault();
+        const { name, value } = e.target;
+        let formErrors = profile.formErrors;
+        let data = profile.data
+        data[name] = value;
+        
+        setProfile({ data, formErrors});
+    }
+
     return (
         <div>
             <Container>
             <Row>
                 <Col>
+                <Form onSubmit={handleSubmit}>
                      <h3>Профаил</h3>
                      <Table striped bordered hover size="sm">
                         <tbody>
                             <tr>
                             <td>Имя</td>
-                            <td>{user.isAuth?
-                                <div>{user.user.id}</div>
-                            :
-                                <div>2</div>}</td>
+                            <td><Form.Control
+                                    name="name"
+                                    type="text"
+                                    onChange={handleChange}
+                                    defaultValue={user.user.name}
+                                />  </td>
                             </tr>
-                        </tbody>
-                    </Table>
-                    <h3>Мои заявки</h3>
-                    <Table striped bordered hover size="sm">
-                        <tbody>
                             <tr>
-                            <td>Имя</td>
-                            <td></td>
+                            <td>Физическое лицо</td>
+                            <td><Form.Check
+                                    name="fiz"
+                                    type="checkbox"
+                                    onChange={handleChange}
+                                    defaultValue={user.user.fiz}
+                                />  </td>
                             </tr>
-                        </tbody>
-                    </Table>
-                    <h3>Мои предложения</h3>
-                    <Table striped bordered hover size="sm">
-                        <tbody>
                             <tr>
-                            <td>Имя</td>
-                            <td></td>
+                            <td>Название организации</td>
+                            <td> <Form.Control
+                                    name="nameOrg"
+                                    type="text"
+                                    onChange={handleChange}
+                                    defaultValue={user.user.nameOrg}
+                                />  </td>
+                            </tr>
+                            <tr>
+                            <td>Адрес организации</td>
+                            <td> <Form.Control
+                                    name="adressOrg"
+                                    type="text"
+                                    onChange={handleChange}
+                                    defaultValue={user.user.adressOrg}
+                                /></td>
+                            </tr>
+                            <tr>
+                            <td>ИНН</td>
+                            <td> <Form.Control
+                                    name="adressOrg"
+                                    type="text"
+                                    onChange={handleChange}
+                                    defaultValue={user.user.inn}
+                                /></td>
+                            </tr>
+                            <tr>
+                            <td>Контактный телефон</td>
+                            <td> 
+                                <Form.Control
+                                    name="telefon"
+                                    type="text"
+                                    onChange={handleChange}
+                                    defaultValue={user.user.telefon}
+                                />  
+                            </td>
                             </tr>
                         </tbody>
                     </Table>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        className="btn btn-success ml-auto mr-1"
+                        >
+                        Сохранить
+                    </Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
-            
         </div>
     );
 });
