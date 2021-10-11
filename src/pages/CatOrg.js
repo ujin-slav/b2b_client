5a048bab867f07de5600c41aae663b015c9753a5
John <ujin_slav@mail.ru>
2021-10-11 23:33:37 +0300

    CatOrg

--- src/pages/CatOrg.js
+++ src/pages/CatOrg.js
@@ -1,23 +1,51 @@
 import React,{useEffect, useState} from 'react';
 import OrgService from '../services/OrgService'
+import {
+    Card,
+    Col,
+    Container,
+    Row
+  } from "react-bootstrap";
 
 const CatOrg = () => {
-    const[org,setOrg] = useState();
+    const[org,setOrg] = useState([]);
 
     useEffect(() => {
         OrgService.getOrg(1,3).then((data)=>{
               setOrg(data.docs);
-              console.log(org)
           })
     },[]);
   
 
     return (
-        <div>
-             {org.map((item)=>{
-             <div>
-                 {item.INN}
-             </div>})}
+        <div >
+            <Container>
+             {org?.map((item)=>
+            <Row style={{ padding: "5px" }}>
+                <Col>
+                    <Card>
+                    <Card.Header>{item.NameOrg}</Card.Header>
+                    <Card.Body>
+                    <Card.Text>
+                            <div>ИНН: {item.INN} КПП:{item.KPP}</div>
+                            <div>{item.Address}</div>
+                            <div>{item.Surname}</div>
+                            <div>{item.Name}</div>
+                            <div>{item.Patron}</div>
+                            <div>{item.Category}</div>
+                            <div>{item.Telefon}</div>
+                            <div>{item.email}</div>
+                            <div>{item.Debt}</div>
+                            <div>{item.Price}</div>
+                            <div>{item.OKPO}</div>
+                            <div>{item.Site}</div> 
+                    </Card.Text>
+                    </Card.Body>
+                    </Card>
+                </Col>
+            </Row>    
+             )} 
+             </Container>
         </div>
     );
 };
