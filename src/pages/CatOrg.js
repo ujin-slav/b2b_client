a971e7db672a2bc52c9acd486a040486ac17501b
John <ujin_slav@mail.ru>
2021-10-13 00:37:58 +0300

    search

--- src/pages/CatOrg.js
+++ src/pages/CatOrg.js
@@ -1,21 +1,6 @@
 import React,{useEffect, useState} from 'react';
 import OrgService from '../services/OrgService'
 import {
-<<<<<<< HEAD
-    Card,
-    Col,
-    Container,
-    Row
-  } from "react-bootstrap";
-
-const CatOrg = () => {
-    const[org,setOrg] = useState([]);
-
-    useEffect(() => {
-        OrgService.getOrg(1,3).then((data)=>{
-              setOrg(data.docs);
-          })
-=======
     Container,
     Row,
     Col,
@@ -53,7 +38,6 @@ const CatOrg = () => {
         return function(){
             document.removeEventListener('scroll',scrollHandler);
         }
->>>>>>> 1b18fc15dd4be535604b7693647de2d65e2411b4
     },[]);
     
     const scrollHandler = (e) =>{
@@ -64,46 +48,22 @@ const CatOrg = () => {
     }
 
     const handleSearch = (e) =>{
-        setSearch(e.target.value)
+        OrgService.getOrg(currentPage,10,e.target.value).then((data)=>{
+            setTotalDocs(data.totalDocs);
+            setOrg(data.docs);
+            setCurrentPage(prevState=>prevState + 1)
+            setSearch(e.target.value)
+        }).finally(()=>setFetching(false))
     }
 
     return (
-<<<<<<< HEAD
-        <div >
-            <Container>
-             {org?.map((item)=>
-            <Row style={{ padding: "5px" }}>
-                <Col>
-                    <Card>
-                    <Card.Header>{item.NameOrg}</Card.Header>
-                    <Card.Body>
-                    <Card.Text>
-                            <div>ИНН: {item.INN} КПП:{item.KPP}</div>
-                            <div>{item.Address}</div>
-                            <div>{item.Surname}</div>
-                            <div>{item.Name}</div>
-                            <div>{item.Patron}</div>
-                            <div>{item.Category}</div>
-                            <div>{item.Telefon}</div>
-                            <div>{item.email}</div>
-                            <div>{item.Debt}</div>
-                            <div>{item.Price}</div>
-                            <div>{item.OKPO}</div>
-                            <div>{item.Site}</div> 
-                    </Card.Text>
-                    </Card.Body>
-                    </Card>
-                </Col>
-            </Row>    
-             )} 
-=======
         <div>
-            <input type="text" onChange={(e)=>handleSearch(e)} />
+            
             <Container>
+            <input type="text" onChange={(e)=>handleSearch(e)} />
             <div class="row gy-3">
                 {org.map((item)=><CardOrg item={item}/>)}
              </div>
->>>>>>> 1b18fc15dd4be535604b7693647de2d65e2411b4
              </Container>
         </div>
     );
