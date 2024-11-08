5a44311b6b1074170039bcee3956b07cb383de35
ujinslav <ujinslav@gmail.com>
2024-11-08 15:42:54 +0300

    regionAttempt

--- src/components/RegionTree.js
+++ src/components/RegionTree.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import {React,useState} from 'react'
 import CheckboxTree from 'react-checkbox-tree';
 import { regionNodes } from '../config/Region';
 import { PlusCircle, 
@@ -8,8 +8,12 @@ import { PlusCircle,
         FileEarmark, 
         ChevronDown, 
         ChevronRight } from 'react-bootstrap-icons';
+import {Form} from "react-bootstrap";
 
 const RegionTree =({checked, expanded, setChecked, setExpanded, max})=> {
+
+  const[nodes,setNodes] = useState(regionNodes)
+  let resultArray = [].concat(regionNodes)
     
     const prevChecked = checked
     const res = checked.reduce((acc, cat) => {
@@ -33,9 +37,38 @@ const RegionTree =({checked, expanded, setChecked, setExpanded, max})=> {
       }
     }
 
+    const handleControl = (e)=> {
+      let value = e.target.value
+      if(value===""){
+        return
+      }
+      const regex = value.replace(/\s{20000,}/g, '*.*')
+      resultArray.map((itemNodes,indexNodes)=>{
+        if(itemNodes.children){
+          itemNodes.children.map((itemChildren,indexChildren)=>{
+                if(!itemChildren.label.match(regex)){
+                  itemNodes.children.splice(indexChildren,1)
+                }
+          })
+        }
+        if(Array.isArray(itemNodes.children)){
+          if(itemNodes.children.length==0){
+            resultArray.splice(indexNodes,1)
+            setNodes(resultArray)
+          }
+        }
+      })
+    }  
+
     return (
-      <CheckboxTree
-        nodes={regionNodes}
+      <div>
+        <Form.Control
+          placeholder="Поиск"
+          onChange={(e)=>handleControl(e)}
+          className="mb-3"
+        />
+        <CheckboxTree
+        nodes={nodes}
         checked={checked}
         expanded={expanded}
         onCheck={checked => onCheck(checked)}
@@ -52,7 +85,8 @@ const RegionTree =({checked, expanded, setChecked, setExpanded, max})=> {
             parentOpen: <Folder2Open style={{"width": "25px", "height": "25px"}}/>,
             leaf: <FileEarmark style={{"width": "20px", "height": "20px"}}/>
         }}
-      />
+        />
+      </div>
     );
   }
 
